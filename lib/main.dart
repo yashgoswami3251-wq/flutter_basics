692e90077b16c14eaca6cc2709f168a0fcef3607
Yashgiri <yashgoswami3251@gmail.com>
2026-06-06 17:35:48 +0530

    Learn application create zero to start main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,34 @@
+// Create demo application fluuter for practice
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-  }
+void main(){
+  runApp(flutterapp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class flutterapp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "FlutterApp",
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        primarySwatch: Colors.grey
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Mydashboardscreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Mydashboardscreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('Flutter container'),
+        title: Text("Dashboard"),
       ),
-      body: Center(
-        child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.black12,
-            child: Text('Hello Devops!'),
-        ),
+      body: Container(
+        color: Colors.lightGreen,
       ),
     );
   }
