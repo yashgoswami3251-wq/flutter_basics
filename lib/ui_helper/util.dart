669baa1c8dc86bb2e1e4cd9f295be498e54b66df
Yashgiri <yashgoswami3251@gmail.com>
2026-06-11 23:16:50 +0530

    Universal theme

---
+++ lib/ui_helper/util.dart
@@ -0,0 +1,24 @@
+// This is global styles file
+
+import 'package:flutter/material.dart';
+
+TextStyle mytextStyle11({Color textColor = Colors.red , FontWeight fontWeight = FontWeight.bold})
+{
+  return TextStyle(
+    fontSize: 11,
+    color: textColor,
+    fontWeight: fontWeight
+  );
+}
+
+TextStyle mytextStyle21(){
+  return TextStyle(
+    fontSize: 21,
+  );
+}
+
+TextStyle mytextStyle16(){
+  return TextStyle(
+    fontSize: 16,
+  );
+}
--- lib/universal_theme.dart
+++ lib/universal_theme.dart
@@ -1,6 +1,5 @@
-// Create demo application fluuter for practice
-
 import 'package:flutter/material.dart';
+import 'package:flutter_basics/ui_helper/util.dart';
 
 void main(){
   runApp(Myflutterapp());
@@ -14,9 +13,14 @@ class Myflutterapp extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primarySwatch: Colors.blue,
+
         textTheme: TextTheme(
+
+          // We can use anywhere this styles in application
+
           headlineLarge: TextStyle(fontSize: 25, fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
-          headlineMedium: TextStyle(fontSize: 15, fontStyle: FontStyle.normal, fontWeight: FontWeight.bold)
+          headlineMedium: TextStyle(fontSize: 15, fontStyle: FontStyle.normal, fontWeight: FontWeight.bold),
+
         )
       ),
       home: DashboardScreen(),
@@ -36,10 +40,13 @@ class DashboardScreen extends StatelessWidget{
       ),
       body:Column(
           children: [
-            Text("Text1",style: Theme.of(context).textTheme.headlineMedium,),
+
+            // Add some other styles extra with global styles then use !.copyWith
+
+            Text("Text1",style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: Colors.white,backgroundColor: Colors.grey),),
             Text("Text1",style: Theme.of(context).textTheme.headlineLarge,),
             Text("Text1",style: Theme.of(context).textTheme.headlineMedium,),
-            Text("Text1",style: Theme.of(context).textTheme.headlineLarge,)
+            Text("Text1",style: mytextStyle11(textColor: Colors.lightBlue))
           ],
         ),
     );
