e86b0dff359df7d37a209c14c106b20574d2f1a8
Yashgiri <yashgoswami3251@gmail.com>
2026-07-10 23:03:11 +0530

    Hero Animation in Flutter

---
+++ lib/Animations/detail_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../Switching_pages/myhome_page.dart';
+import '../animated_container.dart';
+import 'hero_animation.dart';
+
+class MyHomepage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Detail")),
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            InkWell(
+
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>Yash(),));
+              },
+
+              child: Hero(
+                  tag: "background",
+                  child: Image.asset(
+                    "assets/images/lamborghini.png",
+                    height: 400,
+                    width: 400,)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Animations/hero_animation.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_basics/animated_container.dart';
+import '../cross_fade_animated.dart';
+import 'detail_page.dart';
+
+void main(){
+  runApp(myflutter());
+}
+
+class myflutter extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My Flutter",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen
+      ),
+      home: Yash(),
+    );
+  }
+}
+
+class Yash extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Hero Animation")),
+        backgroundColor: Colors.teal,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              
+              onTap: (){
+                Navigator.push(
+                    context, MaterialPageRoute(builder:
+                    (context)=>MyHomepage(),));
+              },
+              
+              child: Hero(
+                  tag: "background",
+                  child: Image.asset(
+                    "assets/images/lamborghini.png",
+                    height: 200,
+                    width: 300,
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
